c9d1d0495b039a9b6e303fcfea920a27247fd340
ecDusty <e.macalligan@outlook.com>
2019-02-08 22:47:04 +0800

    ADD: Streeview API on G-Maps Class

--- src/js/app.js
+++ src/js/app.js
@@ -671,6 +671,11 @@ class G_Model {
     return new google.maps.LatLng(lat,lng);
   }
 
+  streetViewPic(size = '600x400',location = '', params) {
+      params ? params = `&${params}&` : params = '';
+      return `https://maps.googleapis.com/maps/api/streetview?size=${size}&location=${location}${params}&key=${this.API}`
+  }
+
 //Asychronously Load Map scripts
   loadMap(API) {
     const gMaps = `https://maps.googleapis.com/maps/api/js`,
@@ -751,227 +756,228 @@ function FynViewModel() {
 // for INTERFACE
 //
 //
-    self.intViewClass = ko.pureComputed({
-        read: function() {
-            return self.activeMenu() + ' ' + (self.intView() ? 'active' : '');
-        }
+  self.intViewClass = ko.pureComputed({
+    read: function() {
+      return self.activeMenu() + ' ' + (self.intView() ? 'active' : '');
+    }
+  });
+  self.searchBounds = '';
+  self.User = {};
+  self.User.location = {
+    lat: null,
+    lng: null
+  }
+
+  self.getUserLocation = function() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(self.geoSetUserLocation)
+    } else {
+        //console.log('Geolocation is not supported by this browser');
+    }
+  }
+  self.geoSetUserLocation = function(position) {
+    self.User.location.lat = position.coords.latitude;
+    self.User.location.lat = position.coords.lattitude;
+  }
+
+  self.googleSetUserLocation = function(position) {
+    self.User.location.lat = position.geometry.location.lat();
+    self.User.location.lng = position.geometry.location.lng();
+  }
+
+  //Markers
+  self.clearMarkers = function(koArray) {
+    for (const item of koArray()) { 
+      item.marker.setMap(null); 
+    }
+    koArray.removeAll();
+  }
+
+  self.hideMarkers = function(koArray) {
+    for (const item of koArray()) { 
+      item.marker.setMap(null); 
+    }
+  }
+
+  self.showMarkers = function(koArray) {
+    let bounds = G.newBounds();
+    for (const item of koArray()) { 
+      item.marker.setMap(self.G.map); 
+      bounds.extend(item.position);
+    }
+    if (koArray().length == 1) {
+      G.map.setCenter(
+        koArray()[0] ? koArray()[0].position : G.testMarker
+      )
+      G.map.setZoom(
+        koArray()[0] ? 15 : 12
+      );
+    } else if (koArray().length > 1) {
+      G.map.fitBounds(bounds);
+    }
+  }
+
+  // === Primary Class for searchable item ===
+
+  // === HOMES ===
+  // Data & Controls
+  //
+  //
+  //Control Home Interface view
+
+  //Searching for homes
+  self.homeSavedItems = ko.observableArray([]);
+  self.homeActiveItems = ko.observableArray([]);
+
+  self.HomeItem = function(home = { position: {}, title: '', icon: '', iconActiveHover: '', search: '', address: '',  ani: google.maps.Animation.DROP }) {
+    const foo = this;
+    this.search = home.search;
+    this.saved = ko.observable(false);
+    this.position = home.position;
+    this.address = ko.observable(home.address)
+    this.title = home.title;
+    this.defIcon = '/images/home-point.png';
+    this.iconActiveHover = '/images/home-active.png';
+    home.icon = {
+      url: foo.defIcon,
+      size: new google.maps.Size(35,45),
+      origin: new google.maps.Point(0,0),
+      anchor: new google.maps.Point(0,43)
+    }
+
+    this.activeIcon = ko.observable(foo.defIcon);
+
+    this.icon = home.icon;
+
+    this.marker = self.G.createMarker(home);
+
+    this.marker.addListener('mouseover',function() {
+      foo.icon.url = foo.iconActiveHover;
+      foo.activeIcon(foo.icon.url);
+      this.setIcon(foo.icon);
     });
-    self.searchBounds = '';
-    self.User = {};
-    self.User.location = {
-        lat: null,
-        lng: null
-    }
 
-    self.getUserLocation = function() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(self.geoSetUserLocation)
-        } else {
-            //console.log('Geolocation is not supported by this browser');
-        }
-    }
-    self.geoSetUserLocation = function(position) {
-        self.User.location.lat = position.coords.latitude;
-        self.User.location.lat = position.coords.lattitude;
-    }
+    this.marker.addListener('mouseout',function() {
+      foo.icon.url = foo.defIcon;
+      foo.activeIcon(foo.icon.url);
+      this.setIcon(foo.icon);
+    });
 
-    self.googleSetUserLocation = function(position) {
-        self.User.location.lat = position.geometry.location.lat();
-        self.User.location.lng = position.geometry.location.lng();
-    }
+    this.marker.addListener('click',function() {
+      console.log(`${foo.marker}\n${foo.icon}\n${foo.title}`)
+    })
 
-    //Markers
-    self.clearMarkers = function(koArray) {
-        for (const item of koArray()) { 
-            item.marker.setMap(null); 
-        }
-        koArray.removeAll();
-    }
+    this.setHome = function() {
+      if (foo.saved()) {
+        foo.defIcon = '/images/home-point.png';
+        self.homeSavedItems.remove(foo);
+        self.hSearch.subMenu() == 'show-list' ? foo.marker.setMap(null) : foo.marker;
+      } else {
+        foo.defIcon = '/images/home-saved.png';
+        self.homeSavedItems.push(foo);
+      }
 
-    self.hideMarkers = function(koArray) {
-        for (const item of koArray()) { 
-            item.marker.setMap(null); 
-        }
+      foo.activeIcon(foo.defIcon);
+      foo.saved(!foo.saved());
+      foo.icon.url = foo.icon.url == foo.activeIcon ? foo.icon.url : foo.defIcon;
+      foo.marker.setIcon(foo.icon);
     }
+  }
 
-    self.showMarkers = function(koArray) {
+  // ================
+  //    Home Search
+  //    Functionality
+  // ================
+  // This is the Object that controls all Home search functionality
+  self.hSearch = {
+    name: ko.observable(''),
+    search: ko.observable(''),
+    subMenu: ko.observable('add-place'),
+
+    setAdd: function() {
+      self.hideMarkers(self.homeSavedItems);
+      self.showMarkers(self.homeActiveItems);
+      self.hSearch.subMenu('add-place');
+    },
+
+    setList: function() {
+      self.hideMarkers(self.homeActiveItems);
+      self.showMarkers(self.homeSavedItems);
+      self.hSearch.subMenu('show-list');
+    },
+
+    searchMark: function() {
+      self.clearMarkers(self.homeActiveItems)
+      console.log(self.searchBounds)
+      self.G.textFindPlace({
+        location: self.searchBounds,
+        radius: `4000`,
+        query:`${self.hSearch.search()}`
+      }, self.hSearch.addMark);
+    },
+
+    addMark: function(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        console.log(results);
+        let x = 0
         let bounds = G.newBounds();
-        for (const item of koArray()) { 
-            item.marker.setMap(self.G.map); 
-            bounds.extend(item.position);
+        for (const result of results) {
+          bounds.extend(result.geometry.location);
+          setTimeout(function() {
+            const item = new self.HomeItem({
+              position: result.geometry.location,
+              title: result.name,
+              address: result.formatted_address,
+              search: self.hSearch.search()
+            });
+            if (self.hSearch.subMenu() == 'add-place')
+              item.marker.setMap(self.G.map)
+            else
+              item.marker.setMap(null);
+            self.homeActiveItems.push(item);
+          },150*x);
+          x++
         }
-        if (koArray().length == 1) {
-            G.map.setCenter(
-                koArray()[0] ? koArray()[0].position : G.testMarker
-            )
-            G.map.setZoom(
-                koArray()[0] ? 15 : 12
-                );
-        } else if (koArray().length > 1) {
-            G.map.fitBounds(bounds);
+        
+        if (results.length <= 1) {
+          G.map.setCenter(results[0].geometry.location)
+          G.map.setZoom(15);
+        } else {
+          G.map.fitBounds(bounds);
         }
+      } else {
+        // console.log(`${results} \n ${status}`);
+        self.error(status);
+      }
+    },
+
+    clearSearch: function() {
+      self.clearMarkers(self.homeActiveItems)
+      self.hSearch.search('');
     }
+  }
 
-    // === Primary Class for searchable item ===
+  // App Initialization
+  //
+  //
+  self.initApp = function() {
+    G.map = new google.maps.Map(G.mapEl, {
+    center: G.testMarker,
+    zoom: 12,
+    styles: G.mStyles[0],
+    disableDefaultUI:true
+    });
+    gMap = G.map;
+    
+    var HongKong = G.makeLatLng('22.286394','114.149139');
+    self.searchBounds = HongKong;
+  }
 
-    // === HOMES ===
-    // Data & Controls
-    //
-    //
-    //Control Home Interface view
-
-    //Searching for homes
-    self.homeSavedItems = ko.observableArray([]);
-    self.homeActiveItems = ko.observableArray([]);
-
-    self.HomeItem = function(home = { position: {}, title: '', icon: '', iconActiveHover: '', search: '', address: '',  ani: google.maps.Animation.DROP }) {
-        const foo = this;
-        this.search = home.search;
-        this.saved = ko.observable(false);
-        this.position = home.position;
-        this.address = ko.observable(home.address)
-        this.title = home.title;
-        this.defIcon = '/images/home-point.png';
-        this.iconActiveHover = '/images/home-active.png';
-        home.icon = {
-            url: foo.defIcon,
-            size: new google.maps.Size(35,45),
-            origin: new google.maps.Point(0,0),
-            anchor: new google.maps.Point(0,43)
-        }
-
-        this.activeIcon = ko.observable(foo.defIcon);
-
-        this.icon = home.icon;
-
-        this.marker = self.G.createMarker(home);
-
-        this.marker.addListener('mouseover',function() {
-            foo.icon.url = foo.iconActiveHover;
-            foo.activeIcon(foo.icon.url);
-            this.setIcon(foo.icon);
-        });
-
-        this.marker.addListener('mouseout',function() {
-            foo.icon.url = foo.defIcon;
-            foo.activeIcon(foo.icon.url);
-            this.setIcon(foo.icon);
-        });
-
-        this.marker.addListener('click',function() {
-            console.log(`${foo.marker}\n${foo.icon}\n${foo.title}`)
-        })
-
-        this.setHome = function() {
-            if (foo.saved()) {
-                foo.defIcon = '/images/home-point.png';
-                self.homeSavedItems.remove(foo);
-                self.hSearch.subMenu() == 'show-list' ? foo.marker.setMap(null) : foo.marker;
-            } else {
-                foo.defIcon = '/images/home-saved.png';
-                self.homeSavedItems.push(foo);
-            }
-
-            foo.activeIcon(foo.defIcon);
-            foo.saved(!foo.saved());
-            foo.icon.url = foo.icon.url == foo.activeIcon ? foo.icon.url : foo.defIcon;
-            foo.marker.setIcon(foo.icon);
-        }
-    }
-
-    // ================
-    //    Home Search
-    //    Functionality
-    // ================
-    // This is the Object that controls all Home search functionality
-    self.hSearch = {
-        name: ko.observable(''),
-        search: ko.observable(''),
-        subMenu: ko.observable('add-place'),
-
-        setAdd: function() {
-            self.hideMarkers(self.homeSavedItems);
-            self.showMarkers(self.homeActiveItems);
-            self.hSearch.subMenu('add-place');
-        },
-
-        setList: function() {
-            self.hideMarkers(self.homeActiveItems);
-            self.showMarkers(self.homeSavedItems);
-            self.hSearch.subMenu('show-list');
-        },
-
-        searchMark: function() {
-            self.clearMarkers(self.homeActiveItems)
-            console.log(self.searchBounds)
-            self.G.textFindPlace({
-              location: self.searchBounds,
-              radius: `4000`,
-              query:`${self.hSearch.search()}`
-            }, self.hSearch.addMark);
-        },
-
-        addMark: function(results, status) {
-            if (status == google.maps.places.PlacesServiceStatus.OK) {
-                console.log(results);
-                let x = 0
-                let bounds = G.newBounds();
-                for (const result of results) {
-                    bounds.extend(result.geometry.location);
-                    setTimeout(function() {
-                        const item = new self.HomeItem({
-                            position: result.geometry.location,
-                            title: result.name,
-                            address: result.formatted_address,
-                            search: self.hSearch.search()
-                        });
-                        if (self.hSearch.subMenu() == 'add-place')
-                            item.marker.setMap(self.G.map)
-                        else
-                            item.marker.setMap(null);
-                        self.homeActiveItems.push(item);
-                    },150*x);
-                    x++
-                }
-                
-                if (results.length <= 1) {
-                    G.map.setCenter(results[0].geometry.location)
-                    G.map.setZoom(15);
-                } else {
-                    G.map.fitBounds(bounds);
-                }
-            } else {
-                // console.log(`${results} \n ${status}`);
-                self.error(status);
-            }
-        },
-
-        clearSearch: function() {
-            self.clearMarkers(self.homeActiveItems)
-            self.hSearch.search('');
-        }
-    }
-
-    // App Initialization
-    //
-    //
-    self.initApp = function() {
-      G.map = new google.maps.Map(G.mapEl, {
-        center: G.testMarker,
-        zoom: 12,
-        styles: G.mStyles[0],
-        disableDefaultUI:true
-      });
-      gMap = G.map;
-      
-      var HongKong = G.makeLatLng('22.286394','114.149139');
-      self.searchBounds = HongKong;
-    }
-
-  self.G.loadMap(self.G.API).then(self.initApp)
-  .catch(function(response){
-    alert(`Looks like the first URL failed. Time to slowly walk away.\n${response}`);
-  })
+  self.G.loadMap(self.G.API)
+  .then(self.initApp)
+    .catch(function(response){
+      alert(`Looks like the first URL failed. Time to slowly walk away.\n${response}`);
+    })
 }
 var test = new FynViewModel()
 ko.applyBindings(test);
