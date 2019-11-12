ef485f6b56e0fd5f3f0aa49b13587c6c24a55b36
Erin <ecmgit@outlook.com>
2019-11-12 21:38:00 +0800

    CHANGE: Separate components out for easy de-bugging

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,702 @@
-// Custom Code for Find Your Neighbourhood application build
+// =========================
+//
+//     Google Maps
+//     Custom Class
+//
+// =========================
+// ===========
+//
+// Google Maps
+//    Class
+//
+// ===========
+
+class G_Model {
+	constructor(API = 'AIzaSyAaYL795gcBKEjS2Ud2Rb12A7hebgrT-Fc',mapElement = document.getElementById('map')) {
+		// Map Settings
+		//
+		this.API = API;
+		this.testMarker = {lat: 22.2485, lng:114.1542}; //Hong Kong
+		this.map = {};
+		this.mStyles = [
+			{
+				name: 'FYN',
+				styles: [
+					{
+						"elementType": "geometry",
+						"stylers": [
+							{
+							"color": "#ebe3cd"
+							}
+					]
+					},
+					{
+						"elementType": "labels",
+						"stylers": [
+							{
+							"weight": 4
+							}
+					]
+					},
+					{
+						"elementType": "labels.text.fill",
+						"stylers": [
+							{
+							"color": "#523735"
+							}
+					]
+					},
+					{
+					"elementType": "labels.text.stroke",
+					"stylers": [
+						{
+						"color": "#f5f1e6"
+						}
+					]
+					},
+					{
+					"featureType": "administrative",
+					"elementType": "geometry.stroke",
+					"stylers": [
+						{
+						"color": "#c9b2a6"
+						}
+					]
+					},
+					{
+					"featureType": "administrative.land_parcel",
+					"elementType": "geometry.stroke",
+					"stylers": [
+						{
+						"color": "#dcd2be"
+						}
+					]
+					},
+					{
+					"featureType": "administrative.land_parcel",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+						"color": "#ae9e90"
+						}
+					]
+					},
+					{
+					"featureType": "landscape.natural",
+					"elementType": "geometry",
+					"stylers": [
+						{
+						"color": "#dfd2ae"
+						}
+					]
+					},
+					{
+					"featureType": "poi",
+					"elementType": "geometry",
+					"stylers": [
+						{
+						"color": "#dfd2ae"
+						}
+					]
+					},
+					{
+					"featureType": "poi",
+					"elementType": "labels",
+					"stylers": [
+						{
+						"weight": 2
+						}
+					]
+					},
+					{
+					"featureType": "poi",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+						"color": "#93817c"
+						}
+					]
+					},
+					{
+					"featureType": "poi.park",
+					"elementType": "geometry.fill",
+					"stylers": [
+						{
+						"color": "#a5b076"
+						}
+					]
+					},
+					{
+					"featureType": "poi.park",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+						"color": "#447530"
+						}
+					]
+					},
+					{
+					"featureType": "road",
+					"elementType": "geometry",
+					"stylers": [
+						{
+						"color": "#f5f1e6"
+						}
+					]
+					},
+					{
+					"featureType": "road.arterial",
+					"elementType": "geometry",
+					"stylers": [
+						{
+						"color": "#fdfcf8"
+						}
+					]
+					},
+					{
+					"featureType": "road.highway",
+					"elementType": "geometry",
+					"stylers": [
+						{
+						"color": "#f8c967"
+						}
+					]
+					},
+					{
+					"featureType": "road.highway",
+					"elementType": "geometry.stroke",
+					"stylers": [
+						{
+						"color": "#e9bc62"
+						}
+					]
+					},
+					{
+					"featureType": "road.highway.controlled_access",
+					"elementType": "geometry",
+					"stylers": [
+						{
+						"color": "#e98d58"
+						}
+					]
+					},
+					{
+					"featureType": "road.highway.controlled_access",
+					"elementType": "geometry.stroke",
+					"stylers": [
+						{
+						"color": "#db8555"
+						}
+					]
+					},
+					{
+					"featureType": "road.local",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+						"color": "#806b63"
+						}
+					]
+					},
+					{
+					"featureType": "transit.line",
+					"elementType": "geometry",
+					"stylers": [
+						{
+						"color": "#dfd2ae"
+						}
+					]
+					},
+					{
+					"featureType": "transit.line",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+						"color": "#8f7d77"
+						}
+					]
+					},
+					{
+					"featureType": "transit.line",
+					"elementType": "labels.text.stroke",
+					"stylers": [
+						{
+						"color": "#ebe3cd"
+						}
+					]
+					},
+					{
+					"featureType": "transit.station",
+					"elementType": "geometry",
+					"stylers": [
+						{
+						"color": "#dfd2ae"
+						}
+					]
+					},
+					{
+					"featureType": "water",
+					"elementType": "geometry.fill",
+					"stylers": [
+						{
+						"color": "#b9d3c2"
+						}
+					]
+					},
+					{
+					"featureType": "water",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{
+						"color": "#92998d"
+						}
+					]
+					}
+				]
+			},
+			{
+				name: 'Bat Man',
+				styles: [
+					{
+						"featureType": "all",
+						"elementType": "all",
+						"stylers": [
+							{
+								"visibility": "on"
+							}
+						]
+					},
+					{
+						"featureType": "all",
+						"elementType": "labels",
+						"stylers": [
+							{
+								"visibility": "off"
+							},
+							{
+								"saturation": "-100"
+							}
+						]
+					},
+					{
+						"featureType": "all",
+						"elementType": "labels.text.fill",
+						"stylers": [
+							{
+								"saturation": 36
+							},
+							{
+								"color": "#000000"
+							},
+							{
+								"lightness": 40
+							},
+							{
+								"visibility": "off"
+							}
+						]
+					},
+					{
+						"featureType": "all",
+						"elementType": "labels.text.stroke",
+						"stylers": [
+							{
+								"visibility": "off"
+							},
+							{
+								"color": "#000000"
+							},
+							{
+								"lightness": 16
+							}
+						]
+					},
+					{
+						"featureType": "all",
+						"elementType": "labels.icon",
+						"stylers": [
+							{
+								"visibility": "off"
+							}
+						]
+					},
+					{
+						"featureType": "administrative",
+						"elementType": "geometry.fill",
+						"stylers": [
+							{
+								"color": "#000000"
+							},
+							{
+								"lightness": 20
+							}
+						]
+					},
+					{
+						"featureType": "administrative",
+						"elementType": "geometry.stroke",
+						"stylers": [
+							{
+								"color": "#000000"
+							},
+							{
+								"lightness": 17
+							},
+							{
+								"weight": 1.2
+							}
+						]
+					},
+					{
+						"featureType": "landscape",
+						"elementType": "geometry",
+						"stylers": [
+							{
+								"color": "#000000"
+							},
+							{
+								"lightness": 20
+							}
+						]
+					},
+					{
+						"featureType": "landscape",
+						"elementType": "geometry.fill",
+						"stylers": [
+							{
+								"color": "#4d6059"
+							}
+						]
+					},
+					{
+						"featureType": "landscape",
+						"elementType": "geometry.stroke",
+						"stylers": [
+							{
+								"color": "#4d6059"
+							}
+						]
+					},
+					{
+						"featureType": "landscape.natural",
+						"elementType": "geometry.fill",
+						"stylers": [
+							{
+								"color": "#4d6059"
+							}
+						]
+					},
+					{
+						"featureType": "poi",
+						"elementType": "geometry",
+						"stylers": [
+							{
+								"lightness": 21
+							}
+						]
+					},
+					{
+						"featureType": "poi",
+						"elementType": "geometry.fill",
+						"stylers": [
+							{
+								"color": "#4d6059"
+							}
+						]
+					},
+					{
+						"featureType": "poi",
+						"elementType": "geometry.stroke",
+						"stylers": [
+							{
+								"color": "#4d6059"
+							}
+						]
+					},
+					{
+						"featureType": "road",
+						"elementType": "geometry",
+						"stylers": [
+							{
+								"visibility": "on"
+							},
+							{
+								"color": "#7f8d89"
+							}
+						]
+					},
+					{
+						"featureType": "road",
+						"elementType": "geometry.fill",
+						"stylers": [
+							{
+								"color": "#7f8d89"
+							}
+						]
+					},
+					{
+						"featureType": "road.highway",
+						"elementType": "geometry.fill",
+						"stylers": [
+							{
+								"color": "#7f8d89"
+							},
+							{
+								"lightness": 17
+							}
+						]
+					},
+					{
+						"featureType": "road.highway",
+						"elementType": "geometry.stroke",
+						"stylers": [
+							{
+								"color": "#7f8d89"
+							},
+							{
+								"lightness": 29
+							},
+							{
+								"weight": 0.2
+							}
+						]
+					},
+					{
+						"featureType": "road.arterial",
+						"elementType": "geometry",
+						"stylers": [
+							{
+								"color": "#000000"
+							},
+							{
+								"lightness": 18
+							}
+						]
+					},
+					{
+						"featureType": "road.arterial",
+						"elementType": "geometry.fill",
+						"stylers": [
+							{
+								"color": "#7f8d89"
+							}
+						]
+					},
+					{
+						"featureType": "road.arterial",
+						"elementType": "geometry.stroke",
+						"stylers": [
+							{
+								"color": "#7f8d89"
+							}
+						]
+					},
+					{
+						"featureType": "road.local",
+						"elementType": "geometry",
+						"stylers": [
+							{
+								"color": "#000000"
+							},
+							{
+								"lightness": 16
+							}
+						]
+					},
+					{
+						"featureType": "road.local",
+						"elementType": "geometry.fill",
+						"stylers": [
+							{
+								"color": "#7f8d89"
+							}
+						]
+					},
+					{
+						"featureType": "road.local",
+						"elementType": "geometry.stroke",
+						"stylers": [
+							{
+								"color": "#7f8d89"
+							}
+						]
+					},
+					{
+						"featureType": "transit",
+						"elementType": "geometry",
+						"stylers": [
+							{
+								"color": "#000000"
+							},
+							{
+								"lightness": 19
+							}
+						]
+					},
+					{
+						"featureType": "water",
+						"elementType": "all",
+						"stylers": [
+							{
+								"color": "#2b3638"
+							},
+							{
+								"visibility": "on"
+							}
+						]
+					},
+					{
+						"featureType": "water",
+						"elementType": "geometry",
+						"stylers": [
+							{
+								"color": "#2b3638"
+							},
+							{
+								"lightness": 17
+							}
+						]
+					},
+					{
+						"featureType": "water",
+						"elementType": "geometry.fill",
+						"stylers": [
+							{
+								"color": "#24282b"
+							}
+						]
+					},
+					{
+						"featureType": "water",
+						"elementType": "geometry.stroke",
+						"stylers": [
+							{
+								"color": "#24282b"
+							}
+						]
+					},
+					{
+						"featureType": "water",
+						"elementType": "labels",
+						"stylers": [
+							{
+								"visibility": "off"
+							}
+						]
+					},
+					{
+						"featureType": "water",
+						"elementType": "labels.text",
+						"stylers": [
+							{
+								"visibility": "off"
+							}
+						]
+					},
+					{
+						"featureType": "water",
+						"elementType": "labels.text.fill",
+						"stylers": [
+							{
+								"visibility": "off"
+							}
+						]
+					},
+					{
+						"featureType": "water",
+						"elementType": "labels.text.stroke",
+						"stylers": [
+							{
+								"visibility": "off"
+							}
+						]
+					},
+					{
+						"featureType": "water",
+						"elementType": "labels.icon",
+						"stylers": [
+							{
+								"visibility": "off"
+							}
+						]
+					}
+				]
+			}
+		];
+		this.mapEl = mapElement;
+		this.libraries = ['places','geometry'];
+
+		this.Places = null;
+
+		//Marker Settings
+		//
+
+	}
+
+	createMarker(mark = { position: {}, title: '', icon: '', ani: ''}) {
+		const marker = { map: null };
+
+		mark.position ? marker.position = mark.position : mark.position;
+		mark.title ? marker.title = mark.title : mark.position;
+		mark.icon ? marker.icon = mark.icon : mark.icon;
+		mark.ani ? marker.animation = mark.ani : marker.animation = google.maps.Animation.DROP;
+
+		return new google.maps.Marker(marker);
+	}
+
+	textFindPlace(request,callback) {
+		const self = this;
+		if (self.Places == null) {
+			self.Places = new google.maps.places.PlacesService(self.map)
+			return self.Places.textSearch(request,callback)
+		} else {
+			return self.Places.textSearch(request,callback);
+		}
+	}
+
+	newBounds() {
+		return new google.maps.LatLngBounds();
+	}
+
+	// get(libraries){
+	//   fetch(`https`)
+	// }
+
+	makeLatLng(lat,lng) {
+		return new google.maps.LatLng(lat,lng);
+	}
+
+	streetViewPic(location = ``, size = `600x400`, params) {
+		params ? params = `&${params}&` : params = '';
+		return `https://maps.googleapis.com/maps/api/streetview?size=${size}&location=${location}${params}&key=${this.API}`
+	}
+
+	//Asychronously Load Map scripts
+	loadMap(API) {
+		const gMaps = `https://maps.googleapis.com/maps/api/js`,
+			arrayLibraries = [`places`,`geometry`];
+
+		return new Promise(function(resolve, reject){
+			let script = document.createElement('script');
+			script.src = `${gMaps}?libraries=${arrayLibraries.join()}&key=${API}`;
+			script.onload = () => resolve();
+			script.onerror = () => reject(script.src);
+
+			document.head.appendChild(script);
+		})
+	}
+}
+
 
 
 
 // KNOCKOUT CODING SECTION
 //
-
-//Menu Items
-//
+// * It's important to note that Knockout doesn't support the new ES6 class
+// * and my compiler which converts ES6 to ES2015 seems to produce some kind of error
+// * that stops Knockout from working properly, so all Knockout Classes must use the
+// * older ES2015 JS structure, but other pulled in elements / classes can be written
+// * in ES6 without issue.
 
 function FynViewModel() {
   const self = this;
@@ -268,7 +958,7 @@ function FynViewModel() {
 
     checkMarkExist: function(mark) {
       // const foundItem =
-      
+
       if (self.homeSavedItems().length > 1) {
         const matched = ko.utils.arrayFilter(self.hSearch.homeSavedItems, function(item) {
             return item.place_id == mark.place_id;
@@ -363,43 +1053,43 @@ function FynViewModel() {
 
 
 
-  // ========================
-  //   App Initialization
-  // ========================
-  //
-  //
-  self.initApp = function() {
-    const HongKong = G.makeLatLng('22.286394','114.149139');
+  // self.G.loadMap(self.G.API)
+  // .then(self.initApp)
+  //   .catch(function(response){
+  //     alert(`Looks like the first URL failed. This will be due to an issue with your internet connection. You can see the response error here:\n${response}`);
+  //   })
+}
 
 
 
-    G.map = new google.maps.Map(G.mapEl, {
-      center: HongKong,
-      zoom: 12,
-      styles: G.mStyles[0].styles,
-      zoomControl: false,
-      mapTypeControl: false,
-      fullscreenControl: false,
-      streetViewControl: true,
+// ========================
+//   App Initialization
+// ========================
+//
+//
+FynViewModel.prototype.initApp = () => {
+  const HongKong = G.makeLatLng('22.286394','114.149139');
 
-      mapTypeControlOptions: {
-        mapTypeIds: ['fyn', 'batman']
-      }
-    });
-    G.map.mapTypes.set('fyn', new google.maps.StyledMapType(G.mStyles[0].styles, {name: G.mStyles[0].name, typeId: 'terrain'}));
-    G.map.mapTypes.set('batman', new google.maps.StyledMapType(G.mStyles[1].styles, {name: G.mStyles[1].name}));
-    G.map.setMapTypeId('fyn');
-    G.map.mapTypeId = 'terrain';
+  G.map = new google.maps.Map(G.mapEl, {
+    center: HongKong,
+    zoom: 12,
+    styles: G.mStyles[0].styles,
+    zoomControl: false,
+    mapTypeControl: false,
+    fullscreenControl: false,
+    streetViewControl: true,
 
-    // gMap = G.map;
-    self.searchBounds = HongKong;
-  }
+    mapTypeControlOptions: {
+      mapTypeIds: ['fyn', 'batman']
+    }
+  });
+  G.map.mapTypes.set('fyn', new google.maps.StyledMapType(G.mStyles[0].styles, {name: G.mStyles[0].name, typeId: 'terrain'}));
+  G.map.mapTypes.set('batman', new google.maps.StyledMapType(G.mStyles[1].styles, {name: G.mStyles[1].name}));
+  G.map.setMapTypeId('fyn');
+  G.map.mapTypeId = 'terrain';
 
-  self.G.loadMap(self.G.API)
-  .then(self.initApp)
-    .catch(function(response){
-      alert(`Looks like the first URL failed. This will be due to an issue with your internet connection. You can see the response error here:\n${response}`);
-    })
+  // gMap = G.map;
+  self.searchBounds = HongKong;
 }
 
 FynViewModel.prototype.testFeature = function(test) {
@@ -408,3 +1098,9 @@ FynViewModel.prototype.testFeature = function(test) {
 
 const fyn = new FynViewModel()
 ko.applyBindings(fyn);
+
+fyn.G.loadMap(self.G.API)
+.then(self.initApp)
+  .catch(function(response){
+    alert(`Looks like the first URL failed. This will be due to an issue with your internet connection. You can see the response error here:\n${response}`);
+  })
